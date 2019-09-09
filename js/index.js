b82b73796fe4ee076b5b06a01a2a8d7e76462c38
roman1919 <roman.kot.1994@gmail.com>
2019-09-09 09:13:45 +0000

    Sample calculator

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const inputCalc = document.querySelector('.account');
+      resultCalc = document.querySelector('.resault');
+
+function input(i) {
+    inputCalc.value = inputCalc.value + i
+}
+
+
+function result() {
+    if( eval(inputCalc.value) == undefined) {
+        resultCalc.value = '0.00';
+        inputCalc.value = '0.00';
+    }
+    if( eval(inputCalc.value) == Infinity) {
+        resultCalc.value = '0.00';
+        inputCalc.value = '0.00';
+    }
+    resultCalc.value = eval(inputCalc.value);
+    inputCalc.value = eval(inputCalc.value);
+}
+
+function backspace() {
+    inputCalc.value = inputCalc.value.substring(0, inputCalc.value.lenght-1)
+}
+
+function reset(){
+    resultCalc.value = ''
+    inputCalc.value = '0,00'
+}
